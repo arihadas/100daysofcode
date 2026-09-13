3b048831a5b96c88ed6dd6984227c0a6a2e0bd5f
arihadas <Ariha.35355@stu.upes.ac.in>
2026-09-14 00:53:42 +0545

    Add files via upload

---
+++ Q32.c
@@ -0,0 +1,24 @@
+// Write a program to check if a number is a palindrome.
+
+#include <stdio.h>
+
+int main() {
+    int n, original, reverse = 0, digit;
+
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n != 0) {
+        digit = n % 10;
+        reverse = reverse * 10 + digit;
+        n /= 10;
+    }
+
+    if (original == reverse)
+        printf("Palindrome\n");
+    else
+        printf("Not a palindrome\n");
+
+    return 0;
+}
