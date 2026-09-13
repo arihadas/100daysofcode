84ef26cf497eb086add25d539932400f66ad3856
arihadas <Ariha.35355@stu.upes.ac.in>
2026-09-14 00:51:53 +0545

    Add files via upload

---
+++ Q28.c
@@ -0,0 +1,18 @@
+// Write a program to print the product of even numbers from 1 to n.
+
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    long long product = 1;
+
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i += 2) {
+        product = product * i;
+    }
+
+    printf("Product = %lld\n", product);
+
+    return 0;
+}
