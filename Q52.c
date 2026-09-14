9ebc89b0536182c0067ec64676228f49628e7c32
arihadas <Ariha.35355@stu.upes.ac.in>
2026-09-14 21:45:27 +0545

    Add files via upload

---
+++ Q52.c
@@ -0,0 +1,42 @@
+// Write a program to print the following pattern:
+// *
+// *
+// *
+// *
+// 
+// *
+// *
+// *
+// *
+// *
+// 
+// *
+// *
+// *
+// 
+// *
+
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    for (i = 1; i <= 4; i++)
+        printf("*\n");
+
+    printf("\n");
+
+    for (i = 1; i <= 5; i++)
+        printf("*\n");
+
+    printf("\n");
+
+    for (i = 1; i <= 3; i++)
+        printf("*\n");
+
+    printf("\n");
+
+    printf("*\n");
+
+    return 0;
+}
